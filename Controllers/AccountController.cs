0cee763d3255bd59754768c1266bc8fb7f4f266e
Abisagy Nafula <abisakyn@gmail.com>
2024-05-09 14:59:10 +0300

    small changes

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
--- ViewModels/RegisterVM.cs
+++ ViewModels/RegisterVM.cs
@@ -1,6 +1,19 @@
-﻿namespace CustomIdentity.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.ViewModels
 {
     public class RegisterVM
     {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Compare("Password",ErrorMessage ="Password dont match")]
+        public string? ConfirmPassword { get; set; }
+        public string? Address { get; set; }
     }
 }
